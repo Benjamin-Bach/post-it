5763a53118a2d98d0bc6ae335e8f0b6d1370f629
Benjamin-Bach <mail@benjaminbach.fr>
2021-03-10 18:01:20 +0100

    Improve comment system with alpine js

--- post-it.js
+++ post-it.js
@@ -1,5 +1,31 @@
-document.querySelectorAll('#post-it > *').forEach((item, i) => {
-  let target = document.querySelector(item.dataset.target)
-  target.appendChild(item)
-  target.style.position = 'relative'
-});
+
+
+function newPostIt($refs, $event){
+  if(
+    $refs.state.checked
+    && ($event.target.id != 'postitState')
+    && !$event.target.offsetParent.classList.contains('post-it')){
+    $event.preventDefault()
+
+    let target = $event.target.nodeName == 'IMG' ? $event.target.offsetParent : $event.target
+
+    console.log(target)
+    let rect = target.getBoundingClientRect();
+
+
+    let leftPercent = (($event.clientX - rect.left) / rect.width * 100).toFixed(3)
+    let topPercent = (($event.clientY - rect.top) / rect.height * 100).toFixed(3)
+
+    target.style.position = 'relative'
+    let newComment = document.createElement('div')
+        newComment.setAttribute('x-data', '{ open: false }')
+        newComment.classList.add('post-it')
+        newComment.style.top = topPercent + '%'
+        newComment.style.left = leftPercent + '%'
+        newComment.innerHTML = `<div @click="open = true" class="close"></div>
+                                <div x-show="open" @click.away="open = false" contenteditable="true"></div>`
+    target.appendChild(newComment)
+  }else{
+
+  }
+}
